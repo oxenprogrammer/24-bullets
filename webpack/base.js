60fa7cf23d2ff424180041558d0d46abf0df796e
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-14 23:14:23 +0300

    Perf(config): update webpack babel

--- webpack/base.js
+++ webpack/base.js
@@ -13,6 +13,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime'],
+            ],
+          },
         },
       },
       {
