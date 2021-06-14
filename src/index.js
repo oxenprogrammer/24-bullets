9e7387756fdd8cdda5262dad9b59367252bc7a73
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-14 23:41:24 +0300

    Feat(game): add model as globals

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './styles/main.scss';
 
 import Phaser from 'phaser';
+import Model from './Model';
 import config from './Config/config';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
   }
 }
 
