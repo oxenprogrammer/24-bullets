1338502a1ee2ab451cd757eebb33c30227de0791
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-15 11:00:13 +0300

    Feat(game): add physics

--- src/Config/config.js
+++ src/Config/config.js
@@ -5,4 +5,12 @@ export default {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
 };
