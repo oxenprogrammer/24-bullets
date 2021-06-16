d30d67b17a31ea3751a6b0774a7197c4a063977b
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-16 12:49:51 +0300

    Feat(game): add shooter bullet class

---
+++ src/Objects/Bullet.js
@@ -0,0 +1,10 @@
+import 'phaser';
+
+import Entity from './Entity';
+
+export default class Bullet extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'bullet');
+    this.body.velocity.y = -200;
+  }
+}
