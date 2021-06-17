b1daf50ce43c6be2f62f43adb5d58d91467a4538
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-17 04:25:22 +0300

    Feat(game): add post game

--- src/Scenes/GameOverScene.js
+++ src/Scenes/GameOverScene.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser';
+import { getAllScores, postScore } from '../api/leaderboard.service';
+
 import Button from '../Objects/Buttons';
+import UserFormComponent from '../Form/UserForm.component';
 
 export default class GameOverScene extends Phaser.Scene {
   constructor() {
@@ -11,16 +14,53 @@ export default class GameOverScene extends Phaser.Scene {
   }
 
   create() {
-    this.cameras.main.setBackgroundColor(0xffffff);
-    this.text = this.add.text(300, 300, 'GAME OVER');
-    this.text.setColor('red');
-    this.text.setFontSize(36);
+    this.cameras.main.setBackgroundColor(0x000000);
 
     this.yourScore = this.add.text(300, 200, `Your Score: ${this.score}`);
     this.yourScore.setColor('red');
     this.yourScore.setFontSize(24);
 
-    this.gameButton = new Button(this, 400, 400, 'blueButton1', 'blueButton2', 'Play Again', 'Game');
+    // User Form
+    document.body.appendChild(UserFormComponent());
+
+    // Save button
+
+    this.saveButton = this.add.sprite(400, 340, 'blueButton1').setInteractive();
+    this.saveText = this.add.text(200, 500, 'Save', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+
+    const that = this;
+    this.saveButton.on('pointerdown', () => {
+      const playerName = document.querySelector('[name = "name"]').value;
+      const form = document.querySelector('.form');
+      if (form !== null) {
+        form.remove();
+      }
+      postScore(playerName, that.score);
+      getAllScores().then((result) => {
+        that.scene.start('Title', result);
+      });
+    });
+
+    this.saveButton.on(
+      'pointerover',
+      () => {
+        this.saveButton.setTexture('blueButton2');
+      },
+    );
+
+    this.saveButton.on(
+      'pointerout',
+      () => {
+        this.saveButton.setTexture('blueButton1');
+      },
+    );
+
+    Phaser.Display.Align.In.Center(this.saveText, this.saveButton);
+
+    this.gameButton = new Button(this, 400, 420, 'blueButton1', 'blueButton2', 'Play Again', 'Game');
     this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Main Menu', 'Title');
   }
 }
