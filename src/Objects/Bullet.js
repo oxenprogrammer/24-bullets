3abbf372e7779600597ab47bd64403b7b1fbef06
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-20 13:17:29 +0300

    Perf(config) add dist

--- src/Objects/Bullet.js
+++
@@ -1,8 +0,0 @@
-import Entity from './Entity';
-
-export default class Bullet extends Entity {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'bullet');
-    this.body.velocity.y = -200;
-  }
-}
